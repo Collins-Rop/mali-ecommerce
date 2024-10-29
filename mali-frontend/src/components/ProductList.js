b33a3dd5395174ed426c07277eaa78d1c35fd804
Collins-rop <colinrop68@gmail.com>
2024-10-29 14:21:52 +0300

    (feat)products

---
+++ mali-frontend/src/components/ProductList.js
@@ -0,0 +1,27 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+
+const ProductList = () => {
+    const [products, setProducts] = useState([]);
+
+    useEffect(() => {
+        axios.get('http://localhost:8000/api/products')
+            .then(response => setProducts(response.data))
+            .catch(error => console.log(error));
+    }, []);
+
+    return (
+        <div>
+            <h2>Products</h2>
+            <ul>
+                {products.map(product => (
+                    <li key={product.id}>
+                        {product.name} - ${product.price}
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
+};
+
+export default ProductList;
